d1bd00cd66ef7460bbbb044c897f12af9c5c306c
Arnaud Forgues <arnaud.forgues@gmail.com>
2015-09-03 08:46:49 +0200

    Interface basique d'accueil avec tests de listener (qui marchent pas...)

--- app/src/main/java/org/biniou/myrubikscube/MainActivity.java
+++ app/src/main/java/org/biniou/myrubikscube/MainActivity.java
@@ -2,10 +2,17 @@ package org.biniou.myrubikscube;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     //private MyGLSurfaceView mGLView;
+    private TextView title;
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,28 @@ public class MainActivity extends Activity {
         //mGLView = new MyGLSurfaceView(this);
         //setContentView(mGLView);
         setContentView(R.layout.activity_main);
+
+        title = (TextView) findViewById(R.id.textView);
+        start = (Button) findViewById(R.id.start);
+        start.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(MainActivity.this, "Click zy voir !", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+        start.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                title.setText("Hahahah");
+            }
+        });
+
     }
 
+    public void onClick(View view) {
+        title.setText("Finally");
+        Toast.makeText(this, "Tu vas cliquer oui !", Toast.LENGTH_LONG).show();
+    }
 }
